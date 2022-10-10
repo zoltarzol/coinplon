aa7f67863880e0ce190d785bf17e104cb3a627f7
Cedric Duroisin <duroisin@gmail.com>
2022-10-10 15:12:26 +0200

    first commit even if it's late

---
+++ home/models.py
@@ -0,0 +1,39 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+from wagtail.models import Page
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+
+class HomePage(Page):
+    lead_text = models.CharField(
+        max_length = 140,
+        blank = True,
+        help_text = "Sous-titre sous la bannière"
+    )
+    button = models.ForeignKey(
+        'wagtailcore.Page',
+        blank=True,
+        null=True,
+        related_name="+",
+        help_text="Sélectionner une page à relier",
+        on_delete=models.SET_NULL
+    )
+    button_text = models.CharField(
+        max_length=50,
+        default= "En savoir plus...",
+        blank=False,
+        help_text="Bouton pour le texte",
+    )
+    banner_background_image = models.ForeignKey(
+        'wagtailimages.Image',
+        blank=True,
+        null=True,
+        related_name="+",
+        help_text="Bannière d'arrière-plan",
+        on_delete = models.SET_NULL
+    )
+    content_panels = Page.content_panels + [
+        FieldPanel("lead_text"),
+        PageChooserPanel("button"),
+        FieldPanel("button_text"),
+        FieldPanel("banner_background_image")
+    ]
